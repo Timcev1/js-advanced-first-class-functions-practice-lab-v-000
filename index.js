94e92b028f826615790c6b0b291007c4cbe6eb74
Timothy Cevallos <timcev@gmail.com>
2017-10-06 05:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Code your solution in this file!
 function logDriverNames(driver){
-  
+
 }
 
 function logDriversByHometown(driver, location){
+
+}
+
+function driversByRevenue(){
+  
+}
+
+function driversByName(array){
+  
+}
+
+function totalRevenue(driver){
+  
+}
+
+function averageRevenue(driver){
   
 }
